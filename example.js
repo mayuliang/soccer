60d2e0cc46fcb804384055eb3e7cbc421442e3fb
myl <1010358992@qq.com>
2018-09-17 14:32:45 +0800

    修改

--- example.js
+++ example.js
@@ -2,7 +2,7 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const { window } = new JSDOM(`<!DOCTYPE html>`);
-const $ = require('jQuery')(window);
+const $ = require('jquery')(window);
 var nodemailer = require('nodemailer')
 var smtpTransport = require('nodemailer-smtp-transport');
 var config = require('./config');
