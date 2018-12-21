765fac6db8140b1163c651fdbc1817cd903df62b
myl <1010358992@qq.com>
2018-12-21 16:39:57 +0800

    修改格式

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     //邮件配置
- email: {
-     service: '163',
-     user: '13732638674@163.com',
-     pass: 'mayuliang0605',
- }
+    email: {
+        service: '163',
+        user: '13732638674@163.com',
+        pass: 'mayuliang0605',
+    }
 };
